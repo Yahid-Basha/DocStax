c140eb6bf7b5fa5bed237df4bb337300adf5a1a8
krishh <krishnaveni8v@gmail.com>
2024-02-23 19:46:31 +0530

    Fix plugin imports and update dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,8 @@ import Foundation
 import device_info_plus
 import emoji_picker_flutter
 import file_selector_macos
-<<<<<<< HEAD
-=======
 import flutter_web_auth_2
 import package_info_plus
->>>>>>> 6b90a6095b2e0300524674a1927f2ef4d50d10f0
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -22,11 +19,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-<<<<<<< HEAD
-=======
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
->>>>>>> 6b90a6095b2e0300524674a1927f2ef4d50d10f0
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
