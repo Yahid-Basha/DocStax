07e3adf1f2bf58f6ca46248d245422d763f4935e
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2024-02-24 21:24:47 +0530

    added signup login & onboarding templates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/account/signup.dart';
+import 'pages/onboarding/onboardingpage.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  SignupPage(),
+      home:  OnboardingPage(),
     );
   }
 }
--- lib/pages/account/login.dart
+++ lib/pages/account/login.dart
@@ -1,38 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:appwrite/appwrite.dart';
-import 'package:flutter/widgets.dart';
 import '../../pages/homePage.dart';
-import 'package:flutter/gestures.dart';
-import 'login.dart';
 
 Client client = Client();
 Account account = Account(client);
 
-class SignupPage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
-  _SignupPageState createState() => _SignupPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _SignupPageState extends State<SignupPage> {
+class _LoginPageState extends State<LoginPage> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-
-  void _signup() {
-    String email = _emailController.text;
-    String password = _passwordController.text;
-
-    try {
-      final user = account.create(
-        userId: ID.unique(),
-        email: email,
-        password: password,
-        name: 'John Doe',
-      );
-      user.then((value) => print(value.toMap()));
-    } on AppwriteException catch (e) {
-      print(e.message);
-    }
-  }
 
   void _login() {
     String email = _emailController.text;
@@ -70,10 +50,10 @@ class _SignupPageState extends State<SignupPage> {
                 true); // For self signed certificates, only use for development
 
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 204, 188, 233), // Set the background color to lavender
+      backgroundColor: const Color.fromARGB(255, 204, 188, 233),
       appBar: AppBar(
         title: const Text('Signup'),
-        backgroundColor: Color.fromARGB(255, 204, 188, 233),
+        backgroundColor: const Color.fromARGB(255, 204, 188, 233),
       ),
       body: Center(
         child: Card(
@@ -97,44 +77,19 @@ class _SignupPageState extends State<SignupPage> {
                   ),
                   obscureText: true,
                 ),
-                const SizedBox(height: 10.0),
                 ElevatedButton(
-                  onPressed: _signup,
-                  child: const Text('Signup'),
+                  onPressed: _login,
+                  child: const Text('Login'),
                 ),
-                const SizedBox(height: 10.0),
+                const SizedBox(height: 16.0),
                 ElevatedButton(
                   onPressed: _loginWithGoogle,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(255, 47, 33, 243),
                   ),
                   child: const Text(
-                    'Signup with Google',
+                    'Signin with Google',
                     style: TextStyle(color: Colors.amber),
-                  ),
-                ),
-                const SizedBox(height: 10.0),
-                RichText(
-                  text: TextSpan(
-                    text: 'Have an account? ',
-                    style: const TextStyle(color: Colors.black, fontSize: 14.0),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'Login',
-                        style: const TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoginPage()),
-                            );
-                          },
-                      ),
-                    ],
                   ),
                 ),
               ],
