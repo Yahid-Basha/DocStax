c4302bcd8fb8fbb568f7ea6e60bc8e895c5d5f45
Yahid Basha <97111767+Yahid-Basha@users.noreply.github.com>
2024-07-09 15:21:01 +0530

    updated share modal

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Docstax',
       theme: ThemeData(
+        primaryColor: const Color.fromARGB(199, 98, 45, 134),
       ),
       home:  AuthPage(),
     );
